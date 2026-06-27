6d62f5a6b3b087a137196049ba075219ebbe100a
xzm <1192790289@qq.com>
2026-06-28 01:24:57 +0800

    feat: 启用逻辑删除并添加分页依赖

--- src/main/java/com/xzm/aicode/model/entity/App.java
+++ src/main/java/com/xzm/aicode/model/entity/App.java
@@ -1,9 +1,6 @@
 package com.xzm.aicode.model.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -92,6 +89,7 @@ public class App {
      * 是否删除
      */
     @TableField(value = "isDelete")
+    @TableLogic
     private Integer isDelete;
 
     @Override
--- src/main/java/com/xzm/aicode/model/entity/User.java
+++ src/main/java/com/xzm/aicode/model/entity/User.java
@@ -2,6 +2,7 @@ package com.xzm.aicode.model.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.util.Date;
 import lombok.Data;
@@ -67,6 +68,7 @@ public class User {
     /**
      * 是否删除
      */
+    @TableLogic
     private Integer isDelete;
 
 }
